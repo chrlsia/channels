ae74e11ec2f3f88117cd4feecfd1acd7a402e219
Chris Siannas <xrsia@otenet.gr>
2022-01-06 20:39:05 +0200

    Start of the new program with channels

--- main.go
+++ main.go
@@ -1,17 +1,7 @@
 package main
 
-import "fmt"
 
 func main() {
-	go doSomething("Hello, world")
-
-	for {
-		// do something
-	}
+	
 }
 
-func doSomething(s string) {
-	for {
-		fmt.Println("s is", s)
-	}
-}
