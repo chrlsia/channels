3d56e026212752428b202f1454719130557a8e31
Chris Siannas <xrsia@otenet.gr>
2022-01-06 20:33:44 +0200

    First step

--- main.go
+++ main.go
@@ -1,6 +1,17 @@
 package main
 
+import "fmt"
 
 func main() {
-	
+	go doSomething("Hello, world")
+
+	for {
+		// do something
+	}
+}
+
+func doSomething(s string) {
+	for {
+		fmt.Println("s is", s)
+	}
 }
