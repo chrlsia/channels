d0d20852b4b15b22cf59f359f1ae7c81cf9640d5
Chris Siannas <xrsia@otenet.gr>
2022-01-07 08:08:14 +0200

    Channel complete

--- main.go
+++ main.go
@@ -1,14 +1,49 @@
 package main
 
-var keyPressChan chan rune
+import (
+	"fmt"
 
+	"github.com/eiannone/keyboard"
+)
+
+// Declare that KeyPressChan will be chan rune
+var keyPressChan chan rune //1
 
 
 func main() {
-	keyPressChan = make(chan rune)
+	// Use keyPressChan
+	keyPressChan = make(chan rune) //2
+	go listenForKeyPress()//3
+
+	fmt.Println("Press any key or q to quit")//4
+
+	//5
+	_=keyboard.Open()
+	defer func(){
+		keyboard.Close()
+	}()
+	
+	//6
+	for{
+		//get a key and if it is q or Q quit
+		char, _, _:=keyboard.GetSingleKey()
+		if char == 'q' || char=='Q' {
+			break
+		}
+		//key is not q or Q so push it into channel
+		keyPressChan <- char
+	}
+
 }
 
 func listenForKeyPress(){
+	//7
+	for{
+		// push the conetens of channel to key
+		// and display it
+		key:= <-keyPressChan
+		fmt.Println("You pressed ",string(key))
+	}
 
 }
 
